7ae1231592c541747362f3524a40ce2df14acfd3
Itamar-Brito <itamar.brito@gmail.com>
2023-02-09 22:38:20 -0300

    ajustes docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-
+//Create a docker-composer for mysql with network exposing port 3306
   @Get()
   getHello(): string {
     return String(process.env.NODE_ENV);
